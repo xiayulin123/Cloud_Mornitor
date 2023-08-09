bb545d37f8d542a03d409a4b4d9fdc70d4dc2661
xiayulin123 <xiayulinsky@gmail.com>
2023-08-08 23:15:57 -0400

    finish setting file ecr

--- ecr.py
+++ ecr.py
@@ -1,1 +1,14 @@
 import boto3
+
+ecr_clien = boto3.client('ecr')
+repository_name = "my-cloud-native-repo"
+
+response = cecr_lient.create_repository(
+
+    repositoryName=repository_name
+
+)
+
+repository_uri = response['repository']['repositoryUri']
+
+print(repository_uri)
